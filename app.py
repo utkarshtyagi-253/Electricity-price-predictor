d7bfddd720476f20d0b8370418581a319bd9a691
utkarshtyagi-253 <mathematicianlittle@gmail.com>
2025-08-02 08:56:02 +0530

    app(1).py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ st.title("⚡ Electricity Cost Prediction App")
 st.write("Fill in the building/environment details below to predict electricity cost:")
 
 # Input fields
-site_area = st.number_input("Site Area (sq ft)", min_value=0)
-water_consumption = st.number_input("Water Consumption", min_value=0.0)
+site_area = st.number_input("Site Area (sq ft)", min_value=300,max_value=6000)
+water_consumption = st.number_input("Water Consumption", min_value=600,max_value=13000)
 recycling_rate = st.slider("Recycling Rate (%)", min_value=0, max_value=100)
 utilisation_rate = st.slider("Utilisation Rate (%)", min_value=0, max_value=100)
-air_quality_index = st.number_input("Air Quality Index", min_value=0)
-issue_resolution_time = st.number_input("Issue Resolution Time (in days)", min_value=0)
+air_quality_index = st.number_input("Air Quality Index", min_value=0,max_value=200)
+issue_resolution_time = st.number_input("Issue Resolution Time (in days)", min_value=1,max_value=200)
 
 # User-friendly structure type options
 structure_display = st.selectbox(
